7aba37c98f856aceff76fb949e0c6db63f039299
Jakub Klimczewski <kukli8@o2.pl>
2023-06-13 23:32:36 +0200

    login, logout and register funcionalities

--- front/src/account/account.js
+++ front/src/account/account.js
@@ -1,26 +1,24 @@
 import { Link, useNavigate } from "react-router-dom";
-import Cookies from "universal-cookie";
-const cookies = new Cookies();
+import { useKeycloak } from "@react-keycloak/web";
 
 function Account() {
-    const token = cookies.get("TOKEN");
     const navigate = useNavigate();
+    const { keycloak } = useKeycloak();
+    return (
+        <div>
+            {keycloak.authenticated && (
+                <div>
+                    <h1>Hey</h1>
+                    <Link to="/account/game"><button>Play</button></Link>
+                    <Link to="/account/update"><button>Manage acc</button></Link>
+                    <button onClick={() => keycloak.logout()}>Logout</button>
+                </div>
+            )}
+            {keycloak.authenticated === false && (
 
-    const handleLogout = () => {
-        cookies.remove("TOKEN", { path: "/" });
-        navigate("/login");
-    }
-
-    if (token) return (
-        <>
-            <h1>Hey</h1>
-            <Link to="/account/game"><button>Play</button></Link>
-            <Link to="/account/update"><button>Update</button></Link>
-            <button onClick={handleLogout}>Logout</button>
-        </>
-        
-    );
-    else window.location.href = "/login"
-  }
-  
-  export default Account;
+                window.location.href = "/login"
+            )}
+        </div>
+    )
+}
+export default Account;
--- front/src/account/login.js
+++ front/src/account/login.js
@@ -1,69 +1,28 @@
-import { Formik, Field, Form, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
-import axios from "axios";
-import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Cookies from "universal-cookie";
-const cookies = new Cookies();
-
+import { useKeycloak } from "@react-keycloak/web";
 
 function Login() {
-  const token = cookies.get("TOKEN");
-  const [loggedIn, setLoggIn] = useState("");
   const navigate = useNavigate();
-
-  if (!token) return (
-    <>
-      <h1>Login</h1>
-      <Formik
-          initialValues={{ username: '', password: ''}}
-          validationSchema={Yup.object({
-            username: Yup.string()
-              .min(1, 'Must be 1 charater at least')
-              .required('Required'),
-            password: Yup.string()
-            .required('No password provided.') 
-            .min(5, 'Password is too short - should be 5 chars minimum.')
-          })}
-          onSubmit={(values, { setSubmitting }) => {
-            setTimeout(() => {
-                axios.post("https://localhost:5000/login", values)
-                    .then(res => {
-                      setLoggIn(res.data.message);
-                      setTimeout(() => {
-                        cookies.set("TOKEN", res.data.accessToken, {
-                          path: "/",
-                        });
-                        navigate(`/account`);
-                      }, 500);
-                    })
-                    .catch(err => setLoggIn(err.response.data))
-                setSubmitting(false);
-            }, 400);
-          }}
-        >
-          <Form>
-            <label htmlFor="username">Username</label>
-            <Field name="username" type="text" />
-            <div><ErrorMessage name="username" /></div>
-    
-            <label htmlFor="password">Password</label>
-            <Field name="password" type="password" />
-            <div><ErrorMessage name="password" /></div>
-
-            <p></p>
-            <button type="submit">Submit</button>
-            <button type='reset'>Reset</button>
-          </Form>
-        </Formik>
-        {loggedIn}
-        <br></br>
-        <a href="/register">Register</a>
-        <br></br>
-        <a href="/words">Words List</a>
-    </>
+  const { keycloak } = useKeycloak();
+  return (
+    <div>
+        {keycloak.authenticated === false && (
+            <div>
+                <button type="button" onClick={() => keycloak.login()}>
+                    Login
+                </button>
+                <button type="button" onClick={() => keycloak.register()}>
+                    Register
+                </button>
+                <a href="/words">Words List</a>
+            </div>
+        )}
+        {keycloak.authenticated && (
+            
+            window.location.href = "/account"
+        )}
+    </div>
   )
-  else window.location.href = "/account"
 }
 
 export default Login;
