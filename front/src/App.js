5a2d0cb67d0acf912d71b3668dbecc32fbc25210
Jakub Klimczewski <kukli8@o2.pl>
2023-06-13 13:27:50 +0200

    files arrange + keycloack init

--- front/src/App.js
+++ front/src/App.js
@@ -1,26 +1,30 @@
-import Login from "./login";
-import Register from "./register";
+import Login from "./account/login";
+import Register from "./account/register";
 import Game from "./game/game";
-import Account from "./account"
-import Update from "./update"
-import Data from "./data"
+import Account from "./account/account"
+import Update from "./account/update"
+import Data from "./account/data"
 import NotFound from "./notFound"
 import Words from "./words"
 import { Route, Routes } from "react-router";
+import { ReactKeycloakProvider } from "@react-keycloak/web";
+import keycloak from "./Keycloak";
 
 function App() {  
   return (
   <>
-    <Routes>
-      <Route path="/words" element={<Words/>} />
-      <Route path="/login" element={<Login/>} />
-      <Route path="/register" element={<Register/>} />
-      <Route path="/account" element={<Account/>} />
-      <Route path="/account/game" element={<Game/>} />
-      <Route path="/account/update" element={<Update/>} />
-      <Route path="/account/update/data" element={<Data/>} />
-      <Route path="*" element={<NotFound/>} />
-    </Routes>
+    <ReactKeycloakProvider authClient={keycloak} initOptions={{pkceMethod: "S256"}}>
+      <Routes>
+        <Route path="/words" element={<Words/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="/account" element={<Account/>} />
+        <Route path="/account/game" element={<Game/>} />
+        <Route path="/account/update" element={<Update/>} />
+        <Route path="/account/update/data" element={<Data/>} />
+        <Route path="*" element={<NotFound/>} />
+      </Routes>
+    </ReactKeycloakProvider>
   </>
   );
 }
---
+++ front/src/Keycloak.js
@@ -0,0 +1,11 @@
+import Keycloak from "keycloak-js";
+
+const keycloak = new Keycloak({
+ url: "http://localhost:8080/",
+ realm: "project",
+ clientId: "myappclient",
+ pkceMethod: "S256",
+});
+
+
+export default keycloak;
