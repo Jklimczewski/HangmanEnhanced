b4d383ab84cf104e18bb508c27e9d26417b8080e
Jakub Klimczewski <kukli8@o2.pl>
2023-06-19 10:53:26 +0200

    nginx conf and dockerfile changes

--- front/src/Keycloak.js
+++ front/src/Keycloak.js
@@ -1,7 +1,7 @@
 import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
- url: "http://localhost:8080",
+ url: "https://localhost:8843",
  realm: "project",
  clientId: "HangmanApp",
  pkceMethod: "S256",
